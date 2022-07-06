1fba254f9c12f85cb69a1f1c90f0fe98c3a1c8c3
OlegStrezhnev <oleg.strezhnev@yandex.ru>
2022-07-06 21:04:37 +0300

    add example005

---
+++ Example005/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите имя пользователя ");
+string username = Console.ReadLine();
+if (username.ToLower() == "маша")
+{
+    Console.Write("Ура это же Маха ");
+}
+else{
+     Console.Write("Привет , ");
+     Console.WriteLine(username);
+}
