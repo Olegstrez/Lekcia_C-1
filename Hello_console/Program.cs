a2406cfcc1f648436d43c540e6779e6ac73886a5
OlegStrezhnev <oleg.strezhnev@yandex.ru>
2022-07-06 20:21:13 +0300

    update Hello user
    *.rsuser
    *.suo
    *.user
    *.userosscache
    *.sln.docstates
    
    *.userprefs
    
    mono_crash.*
    
    [Dd]ebug/
    [Dd]ebugPublic/
    [Rr]elease/
    [Rr]eleases/
    x64/
    x86/
    [Ww][Ii][Nn]32/
    [Aa][Rr][Mm]/
    [Aa][Rr][Mm]64/
    bld/
    [Bb]in/
    [Oo]bj/
    [Ll]og/
    [Ll]ogs/
    
    .vs/
    
    Generated\ Files/
    
    [Tt]est[Rr]esult*/
    [Bb]uild[Ll]og.*
    
    *.VisualState.xml
    TestResult.xml
    nunit-*.xml
    
    [Dd]ebugPS/
    [Rr]eleasePS/
    dlldata.c
    
    BenchmarkDotNet.Artifacts/
    
    project.lock.json
    project.fragment.lock.json
    artifacts/
    
    ScaffoldingReadMe.txt
    
    StyleCopReport.xml
    
    *_i.c
    *_p.c
    *_h.h
    *.ilk
    *.meta
    *.obj
    *.iobj
    *.pch
    *.pdb
    *.ipdb
    *.pgc
    *.pgd
    *.rsp
    *.sbr
    *.tlb
    *.tli
    *.tlh
    *.tmp
    *.tmp_proj
    *_wpftmp.csproj
    *.log
    *.tlog
    *.vspscc
    *.vssscc
    .builds
    *.pidb
    *.svclog
    *.scc
    
    _Chutzpah*
    
    ipch/
    *.aps
    *.ncb
    *.opendb
    *.opensdf
    *.sdf
    *.cachefile
    *.VC.db
    *.VC.VC.opendb
    
    *.psess
    *.vsp
    *.vspx
    *.sap
    
    *.e2e
    
    $tf/
    
    *.gpState
    
    _ReSharper*/
    *.[Rr]e[Ss]harper
    *.DotSettings.user
    
    _TeamCity*
    
    *.dotCover
    
    .axoCover/*
    !.axoCover/settings.json
    
    coverage*.json
    coverage*.xml
    coverage*.info
    
    *.coverage
    *.coveragexml
    
    _NCrunch_*
    .*crunch*.local.xml
    nCrunchTemp_*
    
    *.mm.*
    AutoTest.Net/
    
    .sass-cache/
    
    [Ee]xpress/
    
    DocProject/buildhelp/
    DocProject/Help/*.HxT
    DocProject/Help/*.HxC
    DocProject/Help/*.hhc
    DocProject/Help/*.hhk
    DocProject/Help/*.hhp
    DocProject/Help/Html2
    DocProject/Help/html
    
    publish/
    
    *.[Pp]ublish.xml
    *.azurePubxml
    *.pubxml
    *.publishproj
    
    PublishScripts/
    
    *.nupkg
    *.snupkg
    **/[Pp]ackages/*
    !**/[Pp]ackages/build/
    *.nuget.props
    *.nuget.targets
    
    csx/
    *.build.csdef
    
    ecf/
    rcf/
    
    AppPackages/
    BundleArtifacts/
    Package.StoreAssociation.xml
    _pkginfo.txt
    *.appx
    *.appxbundle
    *.appxupload
    
    *.[Cc]ache
    !?*.[Cc]ache/
    
    ClientBin/
    ~$*
    *~
    *.dbmdl
    *.dbproj.schemaview
    *.jfm
    *.pfx
    *.publishsettings
    orleans.codegen.cs
    
    Generated_Code/
    
    _UpgradeReport_Files/
    Backup*/
    UpgradeLog*.XML
    UpgradeLog*.htm
    ServiceFabricBackup/
    *.rptproj.bak
    
    *.mdf
    *.ldf
    *.ndf
    
    *.rdl.data
    *.bim.layout
    *.bim_*.settings
    *.rptproj.rsuser
    *- [Bb]ackup.rdl
    *- [Bb]ackup ([0-9]).rdl
    *- [Bb]ackup ([0-9][0-9]).rdl
    
    FakesAssemblies/
    
    *.GhostDoc.xml
    
    .ntvs_analysis.dat
    node_modules/
    
    *.plg
    
    *.opt
    
    *.vbw
    
    *.vbp
    
    *.dsw
    *.dsp
    
    *.ncb
    *.aps
    
    **/*.HTMLClient/GeneratedArtifacts
    **/*.DesktopClient/GeneratedArtifacts
    **/*.DesktopClient/ModelManifest.xml
    **/*.Server/GeneratedArtifacts
    **/*.Server/ModelManifest.xml
    _Pvt_Extensions
    
    .paket/paket.exe
    paket-files/
    
    .fake/
    
    .cr/personal
    
    __pycache__/
    *.pyc
    
    *.tss
    
    *.jmconfig
    
    *.btp.cs
    *.btm.cs
    *.odx.cs
    *.xsd.cs
    
    OpenCover/
    
    ASALocalRun/
    
    *.binlog
    
    *.nvuser
    
    .mfractor/
    
    .localhistory/
    
    .vshistory/
    
    healthchecksdb
    
    MigrationBackup/
    
    .ionide/
    
    FodyWeavers.xsd
    
    .vscode/*
    !.vscode/settings.json
    !.vscode/tasks.json
    !.vscode/launch.json
    !.vscode/extensions.json
    *.code-workspace
    
    .history/
    
    *.cab
    *.msi
    *.msix
    *.msm
    *.msp
    
    *.sln.iml

--- Hello_console/Program.cs
+++ Hello_console/Program.cs
@@ -1,2 +1,5 @@
-﻿
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите имя");
+string username;
+       username = Console.ReadLine();
+        Console.Write("Привет, ");
+        Console.Write(username);
